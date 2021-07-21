a33b36876aa4fcdd889c20483c9f1004da817712
jaemoonnlee <jaemoon.n.lee@gmail.com>
2021-07-21 23:14:36 +0900

    jquery test

--- src/js/kb_p00.js
+++ src/js/kb_p00.js
@@ -1,7 +1,7 @@
 window.onload = function () {
-    let src00;
-    jQuery.get('../../../src/res/kb/p00/src00.txt', function (data) {
-        src00 = data;
-    });
-    $("#srcCode").html(src00);
+    // let src00;
+    // jQuery.get('../../../src/res/kb/p00/src00.txt', function (data) {
+    //     src00 = data;
+    // });
+    $("#srcCode").load('../../../src/res/kb/p00/src00.html');
 }
