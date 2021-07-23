062b18daf27a0e60032c89ad775c10c20c83f841
jaemoonnlee <jaemoon.n.lee@gmail.com>
2021-07-23 11:09:59 +0900

    Update collapsible.js
    test

--- src/js/collapsible.js
+++ src/js/collapsible.js
@@ -2,8 +2,10 @@ window.onload = function () {
     var coll = document.getElementsByClassName("collapsible");
     var i;
 
+    console.log(coll);
     for (i = 0; i < coll.length; i++) {
         coll[i].addEventListener("click", function () {
+            console.log('...')
             this.classList.toggle("active");
             var content = this.nextElementSibling;
             if (content.style.maxHeight) {
