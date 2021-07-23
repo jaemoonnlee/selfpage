be294146bca70721b00d52d51cf3366385395ea5
jaemoonnlee <jaemoon.n.lee@gmail.com>
2021-07-23 13:57:49 +0900

    on it

---
+++ src/js/kb_p04.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+    let basePath = '../../../src/res/kb/p04/';
+    $("#srcCode00").load(basePath + 'src00_cs_helper.html');
+    $("#srcCode01").load(basePath + 'src01_cs_chart.html');
+}
