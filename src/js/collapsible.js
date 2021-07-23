160e99c69e4cadefc611a77ee7f85349f75647ec
jaemoonnlee <jaemoon.n.lee@gmail.com>
2021-07-23 11:21:15 +0900

    test

--- src/js/collapsible.js
+++ src/js/collapsible.js
@@ -1,18 +1,14 @@
-window.onload = function () {
-    var coll = document.getElementsByClassName("collapsible");
-    var i;
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
-    console.log(coll);
-    for (i = 0; i < coll.length; i++) {
-        coll[i].addEventListener("click", function () {
-            console.log('...')
-            this.classList.toggle("active");
-            var content = this.nextElementSibling;
-            if (content.style.maxHeight) {
-                content.style.maxHeight = null;
-            } else {
-                content.style.maxHeight = content.scrollHeight + "px";
-            }
-        });
-    }
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
 }
--- src/js/kb_p00.js
+++ src/js/kb_p00.js
@@ -1,5 +1,4 @@
 window.onload = function () {
-    // let basePath = '../../../src/res/kb/p00/';
-    // $("#srcCode").load(basePath + 'src00.html');
-    $("#srcCode").load('../../../src/res/kb/p00/src00.html');
+    let basePath = '../../../src/res/kb/p00/';
+    $("#srcCode").load(basePath + 'src00.html');
 }
